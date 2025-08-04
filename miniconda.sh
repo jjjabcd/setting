87d4e29861b83450c2080ea9dc6367647771f73a
jjjabcd <rlawlsgurjh@gmail.com>
2025-08-04 13:45:00 +0900

    update

--- miniconda.sh
+++ miniconda.sh
@@ -3,16 +3,17 @@
 set -e  # Exit on any error
 
 INSTALLER=Miniconda3-latest-Linux-x86_64.sh
-INSTALL_PATH="$HOME/rlawlsgurjh/work/miniconda"
+INSTALLER_FILEPATH = "$Home/work/"
+INSTALL_PATH="$HOME/work/miniconda"
 
 echo "== Step 0: Create target install path if not exists =="
 mkdir -p "$INSTALL_PATH"
 
 echo "== Step 1: Download Miniconda installer =="
-wget -nc https://repo.anaconda.com/miniconda/$INSTALLER
+wget -nc https://repo.anaconda.com/miniconda/$INSTALLER_FILEPATH
 
 echo "== Step 2: Make installer executable =="
-chmod +x $INSTALLER
+chmod +x $INSTALLER_FILEPATH
 
 echo "== Step 3: Install Miniconda (no prompt) =="
 bash ./$INSTALLER -b -p "$INSTALL_PATH"
