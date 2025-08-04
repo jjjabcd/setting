5899f544c74bd3cdd2ef567e98773ab669f1e939
jjjabcd <rlawlsgurjh@gmail.com>
2025-08-04 13:38:28 +0900

    modify

--- docker.sh
+++ docker.sh
@@ -24,19 +24,7 @@ sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin d
 echo "== Step 5: Verify Docker installation =="
 sudo docker run hello-world
 
-echo "== Step 6: Install Python packages for ASKCOS =="
-pip install --upgrade pip
-pip install pyyaml requests
-
-echo "== Step 7: Add user to docker group =="
+echo "== Step 6: Add user to docker group =="
 sudo usermod -aG docker $USER
-echo "You must log out and log back in (or run 'newgrp docker') for group changes to take effect."
 
-echo "== Step 8: Clone and deploy ASKCOS =="
-mkdir -p ~/ASKCOSv2
-cd ~/ASKCOSv2
-git clone git@gitlab.com:mlpds_mit/askcosv2/askcos2_core.git
-cd askcos2_core
-make deploy
-
-echo "== All done. ASKCOS core deployed. =="
+echo "== All done =="
